98609daffc2d3bedcde088cfdecbbb6fad174d25
hidehiro okamoto <okagen@uchida.co.jp>
2017-05-05 08:55:44 +0900

    module & scraping

---
+++ Step12_module.py
@@ -0,0 +1,19 @@
+def module_add(x, y):
+    return x + y
+
+def module_sub(x, y):
+    return x - y
+
+# __name__はグローバル変数。自身がスクリプトファイルとして実行されている場合は
+# __name__ == "__main__" となる。
+# モジュールとして実行されている場合は
+# __name__ == "Step12_module" となる。
+print("\n This message is from Step12_module.py [ptrint(__name__)] : " + __name__)
+
+#この条件を設定しておくと、モジュールとして実行されたさいに以下が呼ばれない。
+if __name__ == "__main__":
+    ans = module_add(3, 4)
+    print(ans)
+    ans = module_sub(3, 4)
+    print(ans)
+    
